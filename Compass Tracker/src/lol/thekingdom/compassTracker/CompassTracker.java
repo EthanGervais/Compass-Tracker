4a5e42bb810b6e5639be26d36053d83257436d06
EthanGervais <EthanGervais@users.noreply.github.com>
2020-11-27 16:18:16 -0500

    First Commit
    This commit contains the base setup files for the plugin.

---
+++ Compass Tracker/src/lol/thekingdom/compassTracker/CompassTracker.java
@@ -0,0 +1,22 @@
+package lol.thekingdom.compassTracker;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 
+ * @author Ethan Gervais
+ *
+ */
+public class CompassTracker extends JavaPlugin implements Listener {
+	
+	@Override
+    public void onEnable() {
+		
+    }
+	
+    @Override
+    public void onDisable() {
+
+    }
+}
